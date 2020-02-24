482b9f12bb203f66b4c2619d0753dcae0d6f4267
rasha <angeldream249@yahoo.com>
2020-02-24 06:56:41 -0800

    Done.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-
+import CatComponent from './CatComponent'
+import GraceHopperQuoteComponent from './GraceHopperQuoteComponent'
+import MouseComponent from './MouseComponent'
+import EinsteinQuoteComponent from './EinsteinQuoteComponent'
 class App extends Component {
 	render() {
 		// your code in the return statement below!
 		return (
 			<div className="App">
 				<CatComponent />
-				<GraceHopperQuoteComponent />
+				<EinsteinQuoteComponent />				
+				<MouseComponent />
 				{/* one more component missing */}
 			</div>
 		);
---
+++ src/EinsteinQuoteComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export  default class EinsteinQuoteComponent extends Component {
+  render() {
+    return (
+        <div className="bar" id="hopper">
+        <i><p>"The most dangerous phrase in the language is, ‘We’ve always done it this way.’"</p>
+        </i><p>-Admiral Grace Hopper</p>
+        </div>
+    );
+  }
+}
